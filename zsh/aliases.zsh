919659eca3175dc15dc05e26ab63fffb8d2093b0
batlab <gaby.alcaras@gmail.com>
2016-04-27 21:19:32 +0200

    Neovim settings with zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,11 @@
 alias reload!='. ~/.zshrc'
 
-alias cls='clear' # Good 'ol Clear Screen command
+# Servers, remote machines
+alias batcloud="ssh gabo@batcloud.me -p 10101"
+alias clipper="ssh alcaras@sas.eleves.ens.fr"
+alias trensmissions="ssh -A -t alcaras@sas.eleves.ens.fr ssh -A alcaras@www.trensmissions.ens.fr"
+
+# Ls
+alias lsa="ls -l --block-size=M"
+
+alias nv="nvim"
