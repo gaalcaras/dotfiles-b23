ee527ad90d01e049bb535633517ca8abc3a9f183
batlab <gaby.alcaras@gmail.com>
2016-07-07 00:46:24 +0200

    Setup solarized colorscheme for zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,11 +1,16 @@
-alias reload!='. ~/.zshrc'
+alias reload='source ~/.zshrc'
 
 # Servers, remote machines
 alias batcloud="ssh gabo@batcloud.me -p 10101"
 alias clipper="ssh alcaras@sas.eleves.ens.fr"
 alias trensmissions="ssh -A -t alcaras@sas.eleves.ens.fr ssh -A alcaras@www.trensmissions.ens.fr"
 
-alias lsa="ls -l --block-size=M"
+alias ls="ls -F --color=auto --block-size=M"
+alias la="ls -a"
+alias ll="ls -l"
+alias lla="ll -a"
+alias ldot="ll -d .*"
+
 alias nv="nvim"
 alias mmv='noglob zmv -W'
 
