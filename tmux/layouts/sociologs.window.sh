950f0ef059c0bc1ed77c50076a5de8274f0e5efd
batlab <gaby.alcaras@gmail.com>
2017-02-19 00:45:07 +0100

    Add tmuxifier configuration

---
+++ tmux/layouts/sociologs.window.sh
@@ -0,0 +1,21 @@
+# Set window root path. Default is `$session_root`.
+# Must be called before `new_window`.
+window_root "~/Codecave/sociologs"
+
+# Create new window. If no argument is given, window name will be based on
+# layout file name.
+new_window "Sociologs"
+
+# Split window into panes.
+split_h 45
+split_v 50
+
+# Run commands.
+run_cmd "git status"     # runs in active pane
+run_cmd "./jekyll_build.sh" 2  # runs in pane 1
+
+# Paste text
+send_keys "nv " 1 # paste into pane 1
+
+# Set active pane.
+select_pane 1
