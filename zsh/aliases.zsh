913473aacb762396ad6da00a1a3c2d2ef8584e6a
batlab <gaby.alcaras@gmail.com>
2017-03-03 17:47:15 +0100

    Remove unused aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,6 @@
 alias reload='source ~/.zshrc'
 
 # Servers, remote machines
-alias batcloud="ssh gabo@batcloud.me -p 10101"
-alias clipper="ssh alcaras@sas.eleves.ens.fr"
-alias trensmissions="ssh -A -t alcaras@sas.eleves.ens.fr ssh -A alcaras@www.trensmissions.ens.fr"
 alias zsh_stats="fc -l 1 | awk '{CMD[\$2]++;count++;}END { for (a in CMD)print CMD[a] \" \" CMD[a]/count*100 \"% \" a;}' | grep -v \"./\" | column -c3 -s \" \" -t | sort -nr | nl | head -n20"
 
 alias ls="ls -F --color=auto --block-size=M"
